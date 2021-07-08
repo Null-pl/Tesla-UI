90d65020a78f686341218741f4046f289d705d18
Pedro Lucas Macedo de Moraes <73931509+PedroLucas-M-m@users.noreply.github.com>
2021-07-08 09:56:10 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Page from './Components/Page'
+import { GlobalStyles } from './Styles/GlobalStyles'
+
+function App() {
+  return (
+    <>
+      <Page />
+
+      <GlobalStyles />
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Page/index.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+
+
+import DefaultOverlayContent from '../DefaultOverlayContent'
+import ModelSection from '../Model/ModelSection'
+import ModelsWrapper from '../Model/ModelsWrapper'
+import UniqueOverlay from '../UniqueOverlay'
+
+import { Container, Spacer } from './styles'
+
+const Page: React.FC = () => {
+  return (
+    <Container>
+      <ModelsWrapper>
+        <div>
+          {[
+            'Model One',
+            'Model Two',
+            'Model Three',
+            'Model Four',
+            'Model Five',
+            'Model Six',
+            'Model Seven'
+          ].map(modelName => (
+            <ModelSection
+              key={modelName}
+              className="colored"
+              modelName={modelName}
+              overlayNode={
+                <DefaultOverlayContent
+                  label={modelName}
+                  description="Order Online for Delivery"
+                />
+              }
+            />
+          ))}
+        </div>
+
+        <Spacer />
+
+        <UniqueOverlay />
+      </ModelsWrapper>
+    </Container>
+  )
+}
+
+export default Page
